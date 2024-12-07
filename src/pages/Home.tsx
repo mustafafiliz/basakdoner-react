96015d718e719fb0716df2bbcc767163dbb75a22
Mustafa <79022605+mustafafiliz@users.noreply.github.com>
2024-12-07 19:05:36 +0300

    feat: add react-router and react-helmet and tailwind css

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import { Helmet } from "react-helmet";
+
+const Home = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Başak Döner</title>
+        <meta name="description" content="Nested component" />
+      </Helmet>
+      Home Page
+    </>
+  );
+};
+
+export default Home;
