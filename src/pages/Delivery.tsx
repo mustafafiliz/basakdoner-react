9ae0a31d4cf1b50490543a284155ebf0a4aa67a8
batuhanmeric <Batuhann.mericc@gmail.com>
2024-12-21 00:49:39 +0300

    fix: image

--- src/pages/Delivery.tsx
+++ src/pages/Delivery.tsx
@@ -17,13 +17,18 @@ const Delivery = () => {
         <link rel="canonical" href="/contact" />
       </Helmet>
       <Container>
-        <div className="lg:flex lg:justify-center lg:items-center gap-3">
+        <div className="">
           <div className="flex flex-col lg:gap-8 gap-4 lg:py-10 py-5">
             <div>
               <h2 className="text-center lg:text-2xl text-lg md:text-4xl text-red-800 font-bold lg:mb-10 mb-4">
                 PAKET SERVİS
               </h2>
-              <p className="text-sm md:text-base font-medium text-[#1a1a1a] max-h-[30vh] lg:max-w-[800px] max-w-[400px] overflow-y-hidden">
+              <img
+                src="/images/Kurye.jpg"
+                alt="kurye"
+                className="lg:max-w-[640px] mx-auto aspect-[4/1] object-cover rounded-lg mb-7"
+              />
+              <p className="text-sm md:text-base font-medium text-[#1a1a1a] max-h-[30vh] lg:max-w-full max-w-[400px] overflow-y-hidden">
                 Kadıköy Kozyatağı Mahallesi’nde bulunan lokantamız, en yakın
                 dönerci arayışında olan herkes için doğal bir merkez
                 konumundadır. Kozyatağı’nın yanı sıra Suadiye, Bostancı,
@@ -43,7 +48,7 @@ const Delivery = () => {
               <h3 className="text-red-800 font-bold lg:text-2xl text-base mb-2">
                 Suadiye Döner Servisi
               </h3>
-              <p className="text-sm md:text-base font-medium text-[#1a1a1a] max-h-[30vh] lg:max-w-[800px] max-w-[400px] overflow-y-auto">
+              <p className="text-sm md:text-base font-medium text-[#1a1a1a] max-h-[30vh] lg:max-w-full max-w-[400px] overflow-y-auto">
                 Suadiye Mahallesi, konum olarak Başak Döner'e komşu bir
                 mahalledir. Suadiye sakinleri, et döner ve tavuk döner yemek
                 istedikleri anda, web sitemizin iletişim bölümünde bulunan
@@ -56,7 +61,7 @@ const Delivery = () => {
               <h3 className="text-red-800 font-bold lg:text-2xl text-base mb-2">
                 Bostancı Döner Servisi
               </h3>
-              <p className="text-sm md:text-base font-medium text-[#1a1a1a] max-h-[30vh] lg:max-w-[800px] max-w-[400px] overflow-y-auto">
+              <p className="text-sm md:text-base font-medium text-[#1a1a1a] max-h-[30vh] lg:max-w-full max-w-[400px] overflow-y-auto">
                 Başak Döner, Bostancı Mahallesi'ndeki döner severlere de hizmet
                 vermektedir. Bostancı sakinleri, taze ve kaliteli et
                 dönerlerimizi evlerinde veya iş yerlerinde keyifle tüketebilir.
@@ -69,7 +74,7 @@ const Delivery = () => {
               <h3 className="text-red-800 font-bold lg:text-2xl text-base mb-2">
                 Erenköy Döner Servisi
               </h3>
-              <p className="text-sm md:text-base font-medium text-[#1a1a1a] max-h-[30vh] lg:max-w-[800px] max-w-[400px]overflow-y-auto">
+              <p className="text-sm md:text-base font-medium text-[#1a1a1a] max-h-[30vh] lg:max-w-full max-w-[400px]overflow-y-auto">
                 Erenköy Mahallesi sakinleri, Başak Döner’in geleneksel Türk
                 mutfağındaki uzmanlığıyla hazırlanan dönerlerini kolayca sipariş
                 edebilir. %100 yaprak et döner ve taze garnitürlerimiz, hızlı ve
@@ -81,7 +86,7 @@ const Delivery = () => {
               <h3 className="text-red-800 font-bold lg:text-2xl text-base mb-2">
                 19 Mayıs Döner Servisi
               </h3>
-              <p className="text-sm md:text-base font-medium text-[#1a1a1a] max-h-[30vh] lg:max-w-[800px] max-w-[400px] overflow-y-auto">
+              <p className="text-sm md:text-base font-medium text-[#1a1a1a] max-h-[30vh] lg:max-w-full max-w-[400px] overflow-y-auto">
                 19 Mayıs Mahallesi sakinleri de Başak Döner’in kaliteli ve
                 lezzetli dönerlerini diledikleri zaman sipariş edebilirler. Taze
                 ve hijyenik şartlarda hazırlanan yemeklerimiz, en kısa sürede
@@ -93,7 +98,7 @@ const Delivery = () => {
               <h3 className="text-red-800 font-bold lg:text-2xl text-base mb-2">
                 Altıntepe Döner Servisi
               </h3>
-              <p className="text-sm md:text-base font-medium text-[#1a1a1a] max-h-[30vh] lg:max-w-[800px] max-w-[400px] overflow-y-auto">
+              <p className="text-sm md:text-base font-medium text-[#1a1a1a] max-h-[30vh] lg:max-w-full max-w-[400px] overflow-y-auto">
                 Altıntepe Mahallesi sakinleri, Başak Döner’in lezzetlerini hem
                 evlerinde hem de iş yerlerinde kolayca tüketebilir. Geleneksel
                 tariflerle hazırlanan et dönerlerimiz, hızlı ve özenli bir
@@ -103,12 +108,6 @@ const Delivery = () => {
               </p>
             </div>
           </div>
-
-          <img
-            src="/images/Kurye.jpg"
-            alt="kurye"
-            className="lg:max-w-[650px] max-w-[400px] object-cover rounded-lg mb-7"
-          />
         </div>
       </Container>
     </>
